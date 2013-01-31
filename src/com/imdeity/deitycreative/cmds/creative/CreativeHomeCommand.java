90423d7f6f62eabbca8d36449c9faf3469211ca0
KILL3RTACO <kill3rtaco@gmail.com>
2013-01-30 16:50:37 -0800

    made /creative tp call /creative home instead of vise versa

--- src/com/imdeity/deitycreative/cmds/creative/CreativeHomeCommand.java
+++ src/com/imdeity/deitycreative/cmds/creative/CreativeHomeCommand.java
@@ -19,39 +19,10 @@ public class CreativeHomeCommand extends DeityCommandReceiver {
 
 	@Override
 	public boolean onPlayerRunCommand(Player player, String[] args) {
-		String playername;
-		int which = 0;
-		ArrayList<Plot> plots;
 		if(args.length == 0){
-			playername = player.getName();
-		}else if(args.length == 1){
-			playername = args[0];
+			new CreativeTeleportCommand().onPlayerRunCommand(player, new String[]{player.getName()});
 		}else{
-			playername = args[0];
-			try{
-				which = Integer.parseInt(args[1]);
-			} catch (NumberFormatException e){
-				which = 0;
-			}
-			
-		}
-		plots = DeityCreative.database.getAllPlots(playername);
-		if(plots == null || plots.size() == 0){
-			DeityCreative.plugin.chat.sendPlayerMessage(player, "&cPlayer does not have any plots");
-		}else{
-			if(which == 0) which = plots.size() - 1;
-			Plot plot = plots.get(which);
-			if(plot != null){
-				int x = 0, y = 21, z = 0;
-				x = (int) plot.getMaxPoint().getX() - (plot.getPlotSize() / 2);
-				z = (int) plot.getMaxPoint().getZ() + 1;
-				Location point = new Location(plot.getWorld(), x + .5, y, z + .5, 180, 0);
-				player.teleport(point);
-				DeityCreative.plugin.chat.sendPlayerMessage(player, "&eWelcome to &a" + playername + "&e's Plot!");
-				return true;
-			}else{
-				DeityCreative.plugin.chat.sendPlayerMessage(player, "&cPlot doesn't exist");
-			}
+			new CreativeTeleportCommand().onPlayerRunCommand(player, new String[]{player.getName(), args[0]});
 		}
 //		String sql = "SELECT `id` FROM " + DeityCreative.database.tableName("deity_creative_", "plots") + " WHERE playername = ?";
 //		DatabaseResults query = DeityCreative.database.readEnhanced(sql, playername);
--- src/com/imdeity/deitycreative/cmds/creative/CreativeTeleportCommand.java
+++ src/com/imdeity/deitycreative/cmds/creative/CreativeTeleportCommand.java
@@ -1,9 +1,13 @@
 package com.imdeity.deitycreative.cmds.creative;
 
+import java.util.ArrayList;
+
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
 import com.imdeity.deityapi.api.DeityCommandReceiver;
 import com.imdeity.deitycreative.DeityCreative;
+import com.imdeity.deitycreative.Plot;
 
 public class CreativeTeleportCommand extends DeityCommandReceiver {
 
@@ -14,12 +18,40 @@ public class CreativeTeleportCommand extends DeityCommandReceiver {
 
 	@Override
 	public boolean onPlayerRunCommand(Player player, String[] args) {
-		if (args.length == 1) {
-			return new CreativeHomeCommand().onPlayerRunCommand(player, new String[]{args[0]});
-		} else {
-			DeityCreative.plugin.chat.sendPlayerMessage(player, "You need to enter a playername");
-			return true;
+		String playername;
+		int which = 0;
+		ArrayList<Plot> plots;
+		if(args.length == 0){
+			playername = player.getName();
+		}else if(args.length == 1){
+			playername = args[0];
+		}else{
+			playername = args[0];
+			try{
+				which = Integer.parseInt(args[1]);
+			} catch (NumberFormatException e){
+				which = 0;
+			}
 		}
+		plots = DeityCreative.database.getAllPlots(playername);
+		if(plots == null || plots.size() == 0){
+			DeityCreative.plugin.chat.sendPlayerMessage(player, "&cPlayer does not have any plots");
+		}else{
+			if(which == 0) which = plots.size() - 1;
+			Plot plot = plots.get(which);
+			if(plot != null){
+				int x = 0, y = 21, z = 0;
+				x = (int) plot.getMaxPoint().getX() - (plot.getPlotSize() / 2);
+				z = (int) plot.getMaxPoint().getZ() + 1;
+				Location point = new Location(plot.getWorld(), x + .5, y, z + .5, 180, 0);
+				player.teleport(point);
+				DeityCreative.plugin.chat.sendPlayerMessage(player, "&eWelcome to &a" + playername + "&e's Plot!");
+				return true;
+			}else{
+				DeityCreative.plugin.chat.sendPlayerMessage(player, "&cPlot doesn't exist");
+			}
+		}
+		return true;
 	}
 
 }
